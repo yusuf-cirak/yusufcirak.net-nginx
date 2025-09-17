e7a297843907994a852efb7f229368b48694ecde
Yusuf Çırak <yusuf10901@hotmail.com>
2025-09-17 22:57:05 +0300

    feat(beszel): add docker-compose configuration for beszel service

--- start.sh
+++ start.sh
@@ -1,13 +1,25 @@
-# # Create a folder named "web-server" in the home directory if not exists
-# if [ ! -d ~/web-server ]; then
-#   mkdir ~/web-server
-# fi
-
-# # Copy the files from the current directory to the "web-server" folder
-# cp -r . ~/web-server
-
-# # Change directory to the "web-server" folder
-# cd ~/web-server
-
+# First create the web server if not exist
 # Run the docker-compose.yml file
 docker compose up --force-recreate --build -d
+
+echo "Web server is up and running."
+
+
+# Run beszel compose
+# mkdir -p ./beszel_ssh
+
+# if [ ! -f ./beszel_ssh/id_rsa ]; then
+#   ssh-keygen -t rsa -b 4096 -f ./beszel_ssh/id_rsa -N ""
+# fi
+
+# if [ ! -f ./beszel_ssh/token ]; then
+#   openssl rand -hex 16 > ./beszel_ssh/token
+# fi
+
+# export BESZEL_KEY="$(cat ./beszel_ssh/id_rsa.pub)"
+# export BESZEL_TOKEN="$(cat ./beszel_ssh/token)"
+
+docker compose -f ./beszel/docker-compose.yml up -d
+echo "Beszel is up and running."
+
+# TODO: Add coolify
