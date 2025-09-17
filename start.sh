d66cc958857ad61a1573b74d288cc350a79b0eb3
Yusuf Çırak <yusuf10901@hotmail.com>
2025-09-17 23:13:52 +0300

    fix(start.sh): improve Beszel Hub startup logic to check if already running

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 # First run Beszel Hub
-sudo docker compose -f ./beszel/docker-compose.yml up -d
-echo "Beszel is up and running."
-
+if ! docker ps --format '{{.Names}}' | grep -q '^beszel$'; then
+  docker compose -f ./beszel/docker-compose.yml up -d
+  echo "Beszel is up and running."
+else
+  echo "Beszel is already running."
+fi
 
 # Run the nginx server
 docker compose up --force-recreate --build -d
