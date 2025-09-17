3138c17426bbcf2aa39dff33e9d7dfeb3798a1c2
Yusuf Çırak <yusuf10901@hotmail.com>
2025-09-17 23:07:11 +0300

    fix(start.sh, docker-compose): update startup script for Beszel and add beszel-network to docker-compose

--- start.sh
+++ start.sh
@@ -1,25 +1,11 @@
-# First create the web server if not exist
-# Run the docker-compose.yml file
+# First run Beszel Hub
+docker compose -f ./beszel/docker-compose.yml up -d
+echo "Beszel is up and running."
+
+
+# Run the nginx server
 docker compose up --force-recreate --build -d
 
 echo "Web server is up and running."
-
-
-# Run beszel compose
-# mkdir -p ./beszel_ssh
-
-# if [ ! -f ./beszel_ssh/id_rsa ]; then
-#   ssh-keygen -t rsa -b 4096 -f ./beszel_ssh/id_rsa -N ""
-# fi
-
-# if [ ! -f ./beszel_ssh/token ]; then
-#   openssl rand -hex 16 > ./beszel_ssh/token
-# fi
-
-# export BESZEL_KEY="$(cat ./beszel_ssh/id_rsa.pub)"
-# export BESZEL_TOKEN="$(cat ./beszel_ssh/token)"
-
-docker compose -f ./beszel/docker-compose.yml up -d
-echo "Beszel is up and running."
 
 # TODO: Add coolify
