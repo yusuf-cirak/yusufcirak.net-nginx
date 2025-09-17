275a74d5ecb79fa503e61001b1649a1e91470afa
Yusuf Çırak <yusuf10901@hotmail.com>
2025-09-17 23:25:40 +0300

    refactor(start.sh): reorganize startup script for Beszel Hub and ensure proper file paths

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
-# First run Beszel Hub
+mkdir -p ../web-server
+
+# copy beszel compose file to web-server directory
+cp ./beszel/docker-compose.yml ../web-server/beszel-docker-compose.yml
+cp ./docker-compose.yml ../web-server/docker-compose.yml
+
+# change directory to web-server
+cd ../web-server
 echo "Starting Beszel Hub..."
-docker compose -f ./beszel-hub/docker-compose.yml up -d
+
+docker compose -f ./beszel-docker-compose.yml up -d
 
 # Run the nginx server
 docker compose -f ./docker-compose.yml up --force-recreate --build -d
