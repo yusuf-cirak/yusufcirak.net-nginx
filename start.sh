528f0f14e672766a9aae11dfa61e235a8410d416
Yusuf Çırak <yusuf10901@hotmail.com>
2025-09-17 23:28:17 +0300

    fix(start.sh): add check for running Beszel Hub before starting

--- start.sh
+++ start.sh
@@ -8,10 +8,16 @@ cp ./docker-compose.yml ../web-server/docker-compose.yml
 cd ../web-server
 echo "Starting Beszel Hub..."
 
-docker compose -f ./beszel-docker-compose.yml up -d
+# First run Beszel Hub
+if ! docker ps --format '{{.Names}}' | grep -q '^beszel$'; then
+  docker compose -f ./beszel/docker-compose.yml up -d
+  echo "Beszel is up and running."
+else
+  echo "Beszel is already running."
+fi
 
 # Run the nginx server
-docker compose -f ./docker-compose.yml up --force-recreate --build -d
+docker compose up --force-recreate --build -d
 
 echo "Web server is up and running."
 
