18a1b354a1fdd54c0b9e961089671824f4414803
Yusuf Çırak <yusuf10901@hotmail.com>
2026-03-05 23:36:47 +0300

    add signoz

--- start.sh
+++ start.sh
@@ -4,10 +4,12 @@ mkdir -p ../web-server
 rm -rf ../web-server/beszel
 rm -rf ../web-server/nginx
 rm -rf ../web-server/tirreno
+rm -rf ../web-server/signoz
 
 cp ./beszel ../web-server/beszel -r
 cp ./nginx ../web-server/nginx -r
 cp ./tirreno ../web-server/tirreno -r
+cp ./signoz ../web-server/signoz -r
 
 # change directory to web-server
 cd ../web-server
@@ -20,10 +22,14 @@ docker compose -f beszel/docker-compose.yml up -d
 echo "Starting Tirreno server..."
 docker compose -f tirreno/docker-compose.yml up -d
 
+
+# Run the Signoz server
+echo "Starting Signoz server..."
+docker compose -f signoz/docker-compose.yaml up -d --remove-orphans
 # Run the nginx server
 echo "Starting Nginx server..."
 cd nginx
-docker compose up --build --force-recreate -d
+# docker compose up --build --force-recreate -d
 
 echo "Web server is up and running."
 
