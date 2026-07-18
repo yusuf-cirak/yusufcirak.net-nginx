ebc53ac393121855feacacabd17487f22c0bb20d
Yusuf Çırak <yusuf10901@hotmail.com>
2026-07-18 22:01:53 +0300

    nginx to traefik

--- start.sh
+++ start.sh
@@ -5,11 +5,13 @@ rm -rf ../web-server/beszel
 rm -rf ../web-server/nginx
 rm -rf ../web-server/tirreno
 rm -rf ../web-server/signoz
+rm -rf ../web-server/traefik
 
 cp ./beszel ../web-server/beszel -r
 cp ./nginx ../web-server/nginx -r
 cp ./tirreno ../web-server/tirreno -r
 cp ./signoz ../web-server/signoz -r
+cp ./traefik ../web-server/traefik -r
 
 # change directory to web-server
 cd ../web-server
@@ -26,10 +28,11 @@ docker compose -f tirreno/docker-compose.yml up --build --force-recreate -d
 # Run the Signoz server
 echo "Starting Signoz server..."
 docker compose -f signoz/docker-compose.yaml up --build --force-recreate -d --remove-orphans
-# Run the nginx server
-echo "Starting Nginx server..."
-cd nginx
-docker compose up --build --force-recreate -d
+# Run Traefik (edge proxy). --remove-orphans removes the old nginx container
+# since both compose files share the same project name (web-server).
+echo "Starting Traefik..."
+cd traefik
+docker compose up --force-recreate -d --remove-orphans
 
 echo "Web server is up and running."
 
